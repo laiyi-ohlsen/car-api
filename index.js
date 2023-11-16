2a2091fb299cde1387afa456238317322f9c484a
Lai Yi Ohlsen <laiyi.ohlsen@gmail.com>
2023-11-15 23:29:28 -0500

    updating cors

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 
-
-
 const app = express(); 
 const port = process.env.PORT || 3001 
 app.use(express.json())
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 
 let cars = {
    1: {
