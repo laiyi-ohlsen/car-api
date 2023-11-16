6640aad26a83c44f87b3dffa3a909e373b669ec4
Lai Yi Ohlsen <laiyi.ohlsen@gmail.com>
2023-11-15 21:00:39 -0500

    adding cors

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express'
+import cors from 'cors'
+
+
 
 const app = express(); 
 const port = process.env.PORT || 3001 
 app.use(express.json())
+app.use(cors());
 
 let cars = {
    1: {
